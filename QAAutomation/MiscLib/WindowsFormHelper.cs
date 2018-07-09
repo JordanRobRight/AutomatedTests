22e248ab2fb701f367779eda9c8a7a54f4653e34
DCIM\rkos <rkos@dciartform.com>
2018-07-09 16:21:48 -0500

    Added MiscLIb project

---
+++ QAAutomation/MiscLib/WindowsFormHelper.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows.Forms;
+
+namespace MiscLib
+{
+    public class WindowsFormHelper
+    {
+        public static void SendKeyToForm(string text, bool isWaiting = false)
+        {
+            try
+            {
+                if (isWaiting)
+                {
+                    SendKeys.SendWait(text);
+                }
+                else
+                {
+                    SendKeys.Send(text);
+                }
+            }
+            catch
+            {
+
+            }
+        }
+    }
+}
