213db5285f98e3299896c14e5bf17c98399e71b9
DCIM\rkos <rkos@dciartform.com>
2018-08-28 12:21:57 -0500

    Fixed issue with Url format

--- QAAutomation/Common/LGUtils.cs
+++ QAAutomation/Common/LGUtils.cs
@@ -8,6 +8,11 @@ namespace QA.Automation.Common
         {
             //string tempUrl = url;
             //return string.Equals(env, "Prod", StringComparison.OrdinalIgnoreCase) ? string.Format(tempUrl, "") : string.Format(tempUrl, "-" + env);
+            if (isProdUrl)
+            {
+                return string.Format(url, "." + env);
+
+            }
             return string.Format(url, "-" + env);
         }
     }
