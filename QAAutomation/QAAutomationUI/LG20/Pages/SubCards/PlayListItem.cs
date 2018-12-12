9c239e05c91eb521729d58dc240ddcdde54f6d05
dcimlg\kos <rkos@dciartform.com>
2018-12-12 13:11:10 -0600

    Updated code to commented out unused fields.

--- QAAutomation/QAAutomationUI/LG20/Pages/SubCards/PlayListItem.cs
+++ QAAutomation/QAAutomationUI/LG20/Pages/SubCards/PlayListItem.cs
@@ -9,14 +9,14 @@ namespace QA.Automation.UITests.LG20.Pages.SubCards
     {
 
         private string _favoriteButton = "#assets-container > div.assets-content-wrapper.js-assets-content > div > div > div:nth-child(1) > div > div.\\5b.lgfe-cm-utilities.lgfe-cm-utilities--column.\\5d > div > button:nth-child(1)";
-        private string _editButton = "";
-        private string _trashButton = "";
-        private string _copyButton = "";
-        private string _openButton = "";
-        private string _timeSection = "";
-        private string _playlistInfo = "";
+        //private string _editButton = "";
+        //private string _trashButton = "";
+        //private string _copyButton = "";
+        //private string _openButton = "";
+        //private string _timeSection = "";
+        //private string _playlistInfo = "";
 
-        private string _playListActionArea = "Div where the buttons are";
+        //private string _playListActionArea = "Div where the buttons are";
 
         private IWebDriver _driver;
         public PlayListItem(IWebDriver driver)
